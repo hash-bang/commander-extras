b200d1ea5ec17a660179939719533ef40d48fe10
Matt Carter <m@ttcarter.com>
2022-07-18 10:13:04 +1000

    task: Bump commander version to latest

--- test/index.js
+++ test/index.js
@@ -7,13 +7,15 @@ describe('commander-extras', ()=> {
 	it('should not effect existing functionality', ()=> {
 		var cli = commander
 			.option('-v, --verbose', 'Be verbose')
-			.parse(['node', 'app.js', '-v']);
+			.parse(['node', 'app.js', '-v'])
+			.opts()
 
 		expect(cli).to.have.property('verbose', true);
 	});
 
 	it('should extend the existing help system with .env', ()=> {
 		var cli = commander
+			.option('--verbose', 'Be verbose')
 			.env('FOO', 'Foo desription')
 			.env('BAR', 'Bar desription')
 			.note('Note one')
@@ -22,7 +24,21 @@ describe('commander-extras', ()=> {
 			.note('Note three')
 			.example('exampleProgram foo bar baz')
 			.example('exampleProgram baz bar quz', 'Example alternate usage')
-			.parse(['node', 'app.js', '--help']);
+			.parse(['node', 'app.js'])
+			.opts()
+
+		expect(cli).to.have.property('verbose', true);
+	});
+
+	it('should extend the existing help system with .note', ()=> {
+		var cli = commander
+			.option('--verbose', 'Be verbose')
+			.note('Note one')
+			.note('Note two')
+			.parse(['node', 'app.js'])
+			.opts()
+
+		expect(cli).to.have.property('verbose', true);
 	});
 
 });
